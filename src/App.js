cd57047f0d6740576a34addd9e8588f952ab502e
Pramod <pramodray0215@gmail.com>
2021-01-04 12:32:50 +0530

    React Horizontal Slider

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React from "react";
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 import './App.css';
+
+class Slider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      prevDisable: true,
+      nextDisable:
+        this.refs && this.refs.offsetWidth >= this.refs.scrollWidth
+          ? true
+          : false,
+    };
+  }
+  componentDidMount() {
+    this.checkButtons(this.refs.offsetWidth, this.refs.scrollWidth);
+  }
+  checkButtons = (offsetWidthValue, scrollWidthValue) => {
+    this.setState({
+      prevDisable: this.refs.scrollLeft <= 0 ? true : false,
+      nextDisable:
+        this.refs.scrollLeft + offsetWidthValue >= scrollWidthValue
+          ? true
+          : false,
+    });
+  };
+  render() {
+    const offsetWidthValue = this.refs.offsetWidth,
+      scrollWidthValue = this.refs.scrollWidth;
+    return (
+      <div
+        className="slider-container"
+        ref={(el) => {
+          this.refs = el;
+        }}
+      >
+        <div
+          className={`btn-slider prev ${this.state.prevDisable ? "" : ""}`}
+          disabled={this.state.prevDisable}
+          onClick={() => {
+            this.refs.scrollLeft -= offsetWidthValue;
+            this.checkButtons(offsetWidthValue, scrollWidthValue);
+          }}
+        >
+          <BsChevronLeft size={30} />
+        </div>
+        <div className="slider-wrapper">{this.props.children}</div>
+
+        <div
+          className={`btn-slider next ${this.state.nextDisable ? "" : ""}`}
+          disabled={this.state.nextDisable}
+          onClick={() => {
+            this.refs.scrollLeft += offsetWidthValue;
+            this.checkButtons(offsetWidthValue, scrollWidthValue);
+          }}
+        >
+          <BsChevronRight size={30} />
+        </div>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Slider/>
     </div>
   );
 }
